b68b14007bb4d8abd19c4744f6adeb498471bd7d
Allan Alarcón <samuel.alarcon@outlook.com>
2017-06-24 17:51:37 -0500

    Agregada insertar inicio

--- src/insertarFin.c
+++ src/insertarFin.c
@@ -1,28 +1,32 @@
-include "miLista.h"
+#include "miLista.h"
 
 extern int Lista_InsertarFin(ListaEnlazada *lista, void *objeto)
 {
-	if (lista != NULL)
+	if (lista != NULL) 
 	{
 		ElementoLista *elemento =  (ElementoLista *)malloc(sizeof(ElementoLista));
-		elemento->objeto = objeto;
 
-		if (Lista_Vacia(lista) == 1)
+		if (elemento != NULL)
 		{
-			
-			elemento->siguiente = elemento->anterior = &(lista->ancla);
-			lista->ancla->siguiente = elemento;
-		}
-		else
-		{
-			ElementoLista *ultimo = Lista_Ultimo(lista);
-			ultimo->siguiente = elemento;
-			elemento->anterior = &ultimo;
-			elemento->siguiente = &(lista->ancla);
-		}
+			elemento->objeto = objeto;
 
-		lista->ancla->anterior = elemento;
-		return 1;
+			if (Lista_Vacia(lista) == 1)
+			{
+				elemento->siguiente = elemento->anterior = &(lista->ancla);
+				lista->ancla->siguiente = elemento;
+			}
+			else
+			{
+				ElementoLista *ultimo = Lista_Ultimo(lista);
+				ultimo->siguiente = elemento;
+				elemento->anterior = ultimo;
+				elemento->siguiente = &(lista->ancla);
+			}
+
+			lista->ancla->anterior = elemento;
+			lista->numeroElementos++;
+			return 1;
+		}
 	}
-	return 0;			
+	return -1;			
 }
--- src/insertarInicio.c
+++ src/insertarInicio.c
@@ -1,1 +1,32 @@
+#include "miLista.h"
+
 extern int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto)
+{
+	if (lista != NULL) 
+	{
+		ElementoLista *elemento =  (ElementoLista *)malloc(sizeof(ElementoLista));
+
+		if (elemento != NULL)
+		{
+			elemento->objeto = objeto;
+
+			if (Lista_Vacia(lista) == 1)
+			{
+				elemento->siguiente = elemento->anterior = &(lista->ancla);
+				lista->ancla->siguiente = elemento;
+			}
+			else
+			{
+				ElementoLista *primero = Lista_Primero(lista);
+				primero->anterior = elemento;
+				elemento->siguiente = primero;
+				elemento->anterior = &(lista->ancla);
+			}
+
+			lista->ancla->siguiente = elemento;
+			lista->numeroElementos++;
+			return 1;
+		}
+	}
+	return -1;			
+}
