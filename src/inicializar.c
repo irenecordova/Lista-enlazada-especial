488720d5addfca37a0e2aba339a633181c647663
IreneCordova <irene_cordova@outlook.es>
2017-06-24 13:06:19 -0500

    Creación de las función inicializar y vacia

--- src/inicializar.c
+++ src/inicializar.c
@@ -1,2 +1,18 @@
-extern int Lista_Inicializar(ListaEnlazada *lista);
+#include "miLista.h"
 
+extern int Lista_Inicializar(ListaEnlazada *lista)
+{
+	ElementoLista *elemento= NULL
+	*elemento = (ElementoLista *)malloc(sizeof(ElementoLista));
+
+	if (lista != NULL)
+	{
+		elemento->objeto = NULL;
+		elemento->siguiente = elemento;
+		elemento->anterior = anterior;
+		
+		lista->numeroElementos = 0;
+		lista->ancla = elemento;
+	}
+	return 1;
+}
--- src/vacia.c
+++ src/vacia.c
@@ -1,1 +1,14 @@
+#include "miLista.h"
+
 extern int Lista_Vacia(ListaEnlazada *lista)
+{
+	if (lista != NULL)
+	{
+		if (lista->numeroElementos==0)
+			return 1;
+		else
+			return 0;
+	}
+	else
+		return -1;
+}
